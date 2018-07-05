cf18bdc3ffa6e6280ad1cebf36a3ba1a8573be6e
Scott <scrosehart@gmail.com>
2018-07-05 14:37:49 -0700

    adjusted Moblins stages to have acceleration

--- src/modules/enemies/moblin.js
+++ src/modules/enemies/moblin.js
@@ -42,6 +42,7 @@ class Moblin extends Enemy {
     );
     this.cycleOne = 11;
     this.cycleTwo = 11;
+    this.acceleration = 0.18;
   };
 
   move() {
@@ -50,7 +51,7 @@ class Moblin extends Enemy {
       if (this.cycleOne === 11) {  //Move R
         if (this.x < backgroundMap.width - this.spriteWidth) {
           this.x += this.speed;
-          this.speed += 0.2;
+          this.speed += this.acceleration;
         } else if (this.x >= backgroundMap.width - this.spriteWidth) {
           this.speed = 2.1;
           this.cycleOne++;
@@ -58,78 +59,95 @@ class Moblin extends Enemy {
       } else if (this.cycleOne === 12) {  //Move slightly back L
         if (this.x > backgroundMap.width - this.spriteWidth * 2) {
           this.x -= this.speed;
+          this.speed = this.speed > this.acceleration ? this.speed - this.acceleration * 0.19 : this.speed + this.acceleration;
         } else if (this.x <= backgroundMap.width - this.spriteWidth * 2) {
           this.cycleOne = 21;
         };
       } else if (this.cycleOne === 21) {  //Move Up
         if (this.y > 0) {
           this.y -= this.speed;
+          this.speed += this.acceleration;
         } else if (this.y <= 0) {
+          this.speed = 2.1;
           this.cycleOne++;
         };
       } else if (this.cycleOne === 22) {  //Move slightly back down
         if (this.y < this.spriteHeight) {
           this.y += this.speed;
+          this.speed = this.speed > this.acceleration ? this.speed - this.acceleration * 0.19 : this.speed + this.acceleration;
         } else if (this.y >= this.spriteHeight) {
           this.cycleOne = 31;
         };
       } else if (this.cycleOne === 31) {  //Move Left
         if (this.x > 0) {
           this.x -= this.speed;
+          this.speed += this.acceleration;
         } else if (this.x <= 0) {
+          this.speed = 2.1;
           this.cycleOne++;
         };
       } else if (this.cycleOne === 32) {  //Move slightly back right
         if (this.x < this.spriteWidth * 2) {
           this.x += this.speed;
+          this.speed = this.speed > this.acceleration ? this.speed - this.acceleration * 0.19 : this.speed + this.acceleration;
         } else if (this.x >= this.spriteWidth * 2) {
           this.cycleOne = 41;
         };
       } else if (this.cycleOne === 41) {  //Move down
         if (this.y < backgroundMap.height - this.spriteHeight) {
           this.y += this.speed;
+          this.speed += this.acceleration;
         } else if (this.y >= backgroundMap.height - this.spriteHeight) {
+          this.speed = 2.1;
           this.cycleOne++;
         };
       } else if (this.cycleOne === 42) {  //Move slight back up
         if (this.y > backgroundMap.height - this.spriteHeight * 2) {
           this.y -= this.speed;
+          this.speed = this.speed > this.acceleration ? this.speed - this.acceleration * 0.19 : this.speed + this.acceleration;
         } else if (this.y <= backgroundMap.height - this.spriteHeight * 2) {
           this.cycleOne = 11;
         };
       };
     } else if (this.life > 2) {  //Stage Two
-      this.speed = 2;
       if (this.cycleTwo === 11) {  //Top to Bottom
-        if (this.y < this.spriteHeight * 1.2) {
+        if (this.y < backgroundMap.height + this.spriteHeight * 1.2) {
           this.y += this.speed;
-        } else if (this.y >= this.spriteHeight * 1.2) {
+          this.speed += this.acceleration * 0.15;
+        } else if (this.y >= backgroundMap.height + this.spriteHeight * 1.2) {
           this.x = -this.spriteWidth * 1.24;
           this.y = this.spriteHeight * 1.7;
+          this.speed = 0.5;
           this.cycleTwo++;
         };
       } else if (this.cycleTwo === 12) {  //L to R
-        if (this.x < this.spriteWidth * 1.2) {
+        if (this.x < backgroundMap.width + this.spriteWidth * 1.2) {
           this.x += this.speed;
-        } else if (this.x >= this.spriteWidth * 1.2) {
+          this.speed += this.acceleration * 0.15;
+        } else if (this.x >= backgroundMap.width + this.spriteWidth * 1.2) {
           this.x = this.spriteWidth * 2.3;
           this.y = backgroundMap.height + this.spriteHeight * 0.25;
+          this.speed = 0.5;
           this.cycleTwo++;
         };
       } else if (this.cycleTwo === 13) {  //Bottom to Top
         if (this.y > -this.spriteHeight * 1.23) {
           this.y -= this.speed;
+          this.speed += this.acceleration * 0.15;
         } else if (this.y <= -this.spriteHeight * 1.23) {
           this.x = backgroundMap.width + this.spriteWidth * 1.2;
           this.y = backgroundMap.height - this.spriteHeight * 1.73;
+          this.speed = 0.5;
           this.cycleTwo++;
         };
       } else if (this.cycleTwo === 14) {  //R to L
         if (this.x > -this.spriteWidth * 1.23) {
           this.x -= this.speed;
+          this.speed += this.acceleration * 0.15;
         } else if (this.x <= -this.spriteWidth * 1.23) {
           this.x = backgroundMap.width - this.spriteWidth * 2.5;
           this.y = -this.spriteHeight * 1.23;
+          this.speed = 0.5;
           this.cycleTwo = 11;
         };
       };
