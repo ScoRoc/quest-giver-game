9917c37e4863a033f90fc2e5519d849abf554b6e
Scott <scrosehart@gmail.com>
2018-08-02 22:28:42 -0700

    made showHideQuestFunc closure better and cleaner

--- src/modules/showHideQuests.js
+++ src/modules/showHideQuests.js
@@ -2,24 +2,22 @@ import link from './player.js';
 
 const ul = $('#quests-div ul');
 
-const lq = link.quests;
-
 let showQuests = () => {
-  ul.empty();
+  const lq = link.quests;
   if (lq.length === 0) {
     ul.text('no current quests');
   } else {
     ul.text('');
-  }
-  let quests = lq.forEach(quest => {
-    let li = $('<li></li>').text(quest);
-    li.click(() => {
-      lq.splice(lq.indexOf(quest), 1);
-      ul.empty();
-      showQuests();
+    lq.forEach(quest => {
+      let li = $('<li></li>').text(quest);
+      li.click(() => {
+        lq.splice(lq.indexOf(quest), 1);
+        ul.empty();
+        showQuests();
+      });
+      ul.append(li);
     });
-    ul.append(li);
-  });
+  }
 };
 
 let showHideQuestsFunc = () => {
@@ -31,12 +29,10 @@ let showHideQuestsFunc = () => {
       if (!showHideQuests.showing()) {
         $('#quests-div button').text('Hide');
         showQuests();
-        $('#quests-div ul li').show();
         showHideQuests.flip();
       } else {
         $('#quests-div button').text('Show');
         ul.text('quests hidden');
-        $('#quests-div ul li').hide();
         showHideQuests.flip();
       }
     }
